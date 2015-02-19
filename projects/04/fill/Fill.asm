e78e26e17c547799c23f21ab8d0b46e40a3d29ac
Valentyn Boginskey <poison5151@gmail.com>
2015-02-19 16:59:39 -0500

    Complete project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,57 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+    // Initialize counter to first pixel
+    @SCREEN
+    D=A
+    @pixel
+    M=D
+
+(LOOP)
+    @KBD
+    D=M
+    @CLEAR
+    D;JEQ
+    @FILL
+    D;JNE
+
+(CLEAR)
+    // Clear pixel on screen
+    @pixel
+    A=M
+    M=0
+
+    // Check if we are already on the first pixel
+    @pixel
+    D=M
+    @SCREEN
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    // Decrement pixel
+    @pixel
+    M=M-1
+    @LOOP
+    0;JMP
+
+(FILL)
+    // Fill pixel on screen
+    D=-1
+    @pixel
+    A=M
+    M=D
+
+    // Check if we are on the last pixel
+    @pixel
+    D=M
+    @24575
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    // Increment pixel
+    @pixel
+    M=M+1
+    @LOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    // Initialize R2 with 0
+    @R2
+    M=0
+    // End if R0 == 0
+    @R1
+    D=M
+    @END
+    D;JEQ
+(LOOP)
+    // End if R1 == 0
+    @R1
+    D=M
+    @END
+    D;JEQ
+    // R2 += R0
+    @R0
+    D=M
+    @R2
+    M=D+M
+    // R1--
+    @R1
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
